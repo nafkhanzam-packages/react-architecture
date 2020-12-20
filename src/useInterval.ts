e54815fe1916233cdc9ec1fb81bb62b0050dd56e
nafkhanzam <nafkhanalzamzami@gmail.com>
2020-12-20 16:09:42 +0700

    switch deps and delay

--- src/useInterval.ts
+++ src/useInterval.ts
@@ -1,10 +1,10 @@
 import {DependencyList, useCallback, useEffect} from "react";
 
-export function useInterval(callback: () => void, delay: number, deps: DependencyList) {
+export function useInterval(callback: () => void, deps: DependencyList, delay: number) {
   const savedCallback = useCallback(callback, [callback, ...deps]);
 
   useEffect(() => {
-    if (delay) {
+    if (delay > 0) {
       const id = setInterval(savedCallback, delay);
       return () => clearInterval(id);
     }
