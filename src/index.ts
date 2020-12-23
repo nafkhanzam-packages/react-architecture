b81bdd2a9781d137b66a8c08c65b7f04d4f56428
nafkhanzam <nafkhanalzamzami@gmail.com>
2020-12-23 17:59:36 +0700

    add storage

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./ComponentPhase";
 export * from "./dummyAsync";
 export * from "./useHandleCallback";
 export * from "./useInterval";
+export * from "./storage";
 export * from "./contexts/createHandledContext";
 export * from "./contexts/BaseAuthContext";
