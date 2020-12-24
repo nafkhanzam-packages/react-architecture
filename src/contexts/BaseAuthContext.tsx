ef2ebf19459fc754e871287aea6380b5c21367fe
nafkhanzam <nafkhanalzamzami@gmail.com>
2020-12-24 14:07:23 +0700

    add public context on authcontext

--- src/contexts/BaseAuthContext.tsx
+++ src/contexts/BaseAuthContext.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren, Provider, useCallback, useEffect, useState} from "react";
+import React, {PropsWithChildren, Provider, useCallback, useEffect, useMemo, useState} from "react";
 import {ComponentPhase} from "../ComponentPhase";
 
 type Action<LoggedType> = {
@@ -6,27 +6,32 @@ type Action<LoggedType> = {
   logout: () => void;
 };
 
-export type AuthContext<LoggedType> = {
-  actions: Action<LoggedType>;
-  logged: LoggedType | null;
-};
-
-export type LoggedAuthContext<LoggedType> = {
+export type LoggedAuthContext<LoggedType, ContextType> = {
   actions: Action<LoggedType>;
   logged: LoggedType;
+  context: ContextType;
 };
 
-export type AuthFC<LoggedType, Props> = React.FC<{auth: AuthContext<LoggedType>} & Props>;
-export type LoggedAuthFC<LoggedType, Props> = React.FC<
-  {auth: LoggedAuthContext<LoggedType>} & Props
+export type AuthContext<LoggedType, ContextType> = {
+  actions: Action<LoggedType>;
+  logged: LoggedType | null;
+  context: ContextType;
+};
+
+export type AuthFC<LoggedType, ContextType, Props> = React.FC<
+  {auth: AuthContext<LoggedType, ContextType>} & Props
+>;
+export type LoggedAuthFC<LoggedType, ContextType, Props> = React.FC<
+  {auth: LoggedAuthContext<LoggedType, ContextType>} & Props
 >;
 
-export const AuthProvider = <LoggedType,>(
+export const AuthProvider = <LoggedType, ContextType>(
   props: PropsWithChildren<{
-    BaseAuthProvider: Provider<AuthContext<LoggedType> | undefined>;
+    BaseAuthProvider: Provider<AuthContext<LoggedType, ContextType> | undefined>;
     getSavedLogged: () => Promise<LoggedType | null>;
     saveLogged: (logged: LoggedType) => Promise<void>;
     onError?: (err: unknown) => void;
+    getContext: (logged: LoggedType | null) => ContextType;
   }>,
 ) => {
   const [logged, setLogged] = useState<LoggedType | null>(null);
@@ -54,16 +59,18 @@ export const AuthProvider = <LoggedType,>(
     })();
   }, [login, logout, props]);
 
+  const contextInstance = useMemo(() => props.getContext(logged), [logged, props]);
+
   const actions: Action<LoggedType> = {login, logout};
-  const context: AuthContext<LoggedType> = {actions, logged};
+  const context: AuthContext<LoggedType, ContextType> = {actions, logged, context: contextInstance};
 
   return <props.BaseAuthProvider value={context}>{props.children}</props.BaseAuthProvider>;
 };
 
-export const createWithAuthContextWrapper = <LoggedType,>(
+export const createWithAuthContextWrapper = <LoggedType, ContextType>(
   phase: ComponentPhase,
-  useAuthContext: () => AuthContext<LoggedType> | undefined,
-) => <Props,>(Component: AuthFC<LoggedType, Props>): React.FC<Props> => (props) => {
+  useAuthContext: () => AuthContext<LoggedType, ContextType> | undefined,
+) => <Props,>(Component: AuthFC<LoggedType, ContextType, Props>): React.FC<Props> => (props) => {
   const authContext = useAuthContext();
 
   const [comp] = phase.useAsync(async () => {
@@ -76,11 +83,13 @@ export const createWithAuthContextWrapper = <LoggedType,>(
   return comp;
 };
 
-export const createWithLoggedAuthContextWrapper = <LoggedType,>(
+export const createWithLoggedAuthContextWrapper = <LoggedType, ContextType>(
   phase: ComponentPhase,
-  useAuthContext: () => AuthContext<LoggedType> | undefined,
+  useAuthContext: () => AuthContext<LoggedType, ContextType> | undefined,
   onNotLogged: () => Promise<void>,
-) => <Props,>(Component: LoggedAuthFC<LoggedType, Props>): React.FC<Props> => (props) => {
+) => <Props,>(Component: LoggedAuthFC<LoggedType, ContextType, Props>): React.FC<Props> => (
+  props,
+) => {
   const authContext = useAuthContext();
 
   const [comp] = phase.useAsync(async () => {
